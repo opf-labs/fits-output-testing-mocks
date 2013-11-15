c3281dc16b9b1163a1dd59f8b20bf11b3e9a49da
carlwilson <carl@openplanetsfoundation.org>
2013-11-15 08:36:30 +0000

    Initial cut of FITS Output Testing Mock.

---
+++ src/main/java/org/opf_labs/testing/FitsOutputTestingMock.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package org.opf_labs.testing;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * @author carl
+ *
+ */
+public final class FitsOutputTestingMock {
+
+	/**
+	 * So a test mock for the tester 
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		// If there's insufficient args throw a bad error 
+		if (args.length < 3) {
+			System.err.println("Insufficient args");
+			System.exit(-1);
+		}
+		File comparisonOne = new File(args[0]);
+		File comparisonTwo = new File(args[1]);
+		String shaTested = args[2];
+		if (!comparisonOne.isDirectory() || !comparisonTwo.isDirectory()) {
+			System.err.println("Expecting two directories to compare");
+			System.exit(-1);
+		}
+		System.out.println("Testing commit " + shaTested);
+		// Execute with OK for now
+		System.exit(testOutputDirs(comparisonOne, comparisonTwo));
+	}
+	
+	private static int testOutputDirs(File masterOutput, File testOutput) {
+		File[] masterFiles = masterOutput.listFiles();
+		File[] testFiles = testOutput.listFiles();
+		if ((masterFiles.length < 1) || (testFiles.length < 1)) {
+			return 125;
+		}
+		if (!(masterFiles.length == testFiles.length)) {
+			System.err.println("Test failed masterFiles:" + masterFiles.length + ", testFiles:" + testFiles.length);
+			return 1;
+		}
+		
+		Map<String, File> testMap = new HashMap<>();
+		for (File test : testFiles) {
+			if (test.isFile()) {
+				String name = test.getAbsolutePath().replace(testOutput.getAbsolutePath(), ".");
+				testMap.put(name, test);
+			}
+		}
+		for (File master : masterFiles) {
+			if (master.isFile()) {
+				String name = master.getAbsolutePath().replace(masterOutput.getAbsolutePath(), ".");
+				if (! testMap.containsKey(name)) {
+					System.err.println("Test failed, test set didn't contain file " + name);
+					return 1;
+				}
+				File test = testMap.get(name);
+				if (!(master.length() == test.length())) {
+					System.err.println("Test failed, test set and master lengths differ for " + name);
+					return 1;
+				}
+				try (InputStream inMaster = new FileInputStream(master); InputStream inTest = new FileInputStream(test)) {
+					String masterMd5 = DigestUtils.md5Hex(inMaster);
+					String testMd5 = DigestUtils.md5Hex(inTest);
+					if (!masterMd5.equals(testMd5)) {
+						System.err.println("Test failed, test set and master MD5s differ for " + name);
+						return 1;
+					}
+				} catch (IOException excep) {
+					System.err.println("Error reading digest, test abandoned.");
+					excep.printStackTrace();
+					return 125;
+				}
+			}
+		}
+		return 0;
+	}
+
+}
